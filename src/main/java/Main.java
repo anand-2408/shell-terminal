37b4e3098f13e853e7024e854a4826e62cc53a1e
Anand Sherikar <anand007sherikar@gmail.com>
2025-03-27 18:39:00 +0530

    single quotes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -10,45 +8,38 @@ public class Main {
         List<String> builtins = builtins();
         String dir = Path.of("").toAbsolutePath().toString();
 
-        while(true) {
+        while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            String[] str = input.split(" ");
-            String command = str[0];
-            String parameter = "";
+            List<String> tokens = parseInput(input);
 
-            if(str.length > 2) {
-                for(int i = 1; i < str.length; i++) {
-                    if(i < str.length-1) {
-                        parameter += str[i] + " ";
-                    } else {
-                        parameter += str[i];
-                    }
-                }
-            } else if(str.length > 1) {
-                parameter = str[1];
+            if (tokens.isEmpty()) {
+                continue;
             }
 
-            switch(command) {
+            String command = tokens.get(0);
+            String[] arguments = tokens.subList(1, tokens.size()).toArray(new String[0]);
+
+            switch (command) {
                 case "exit":
-                    if(parameter.equals("0")) {
+                    if (arguments.length > 0 && arguments[0].equals("0")) {
                         System.exit(0);
                     } else {
                         System.out.println(input + ": command not found");
                     }
                     break;
                 case "echo":
-                    System.out.println(parameter);
+                    System.out.println(String.join(" ", arguments));
                     break;
                 case "type":
-                    if (builtins.contains(parameter)) {
-                        System.out.println(parameter + " is a shell builtin");
+                    if (builtins.contains(arguments[0])) {
+                        System.out.println(arguments[0] + " is a shell builtin");
                     } else {
-                        String path = getPath(parameter);
+                        String path = getPath(arguments[0]);
                         if (path != null) {
-                            System.out.println(parameter + " is " + path);
+                            System.out.println(arguments[0] + " is " + path);
                         } else {
-                            System.out.println(parameter + ": not found");
+                            System.out.println(arguments[0] + ": not found");
                         }
                     }
                     break;
@@ -56,33 +47,35 @@ public class Main {
                     System.out.println(dir);
                     break;
                 case "cd":
-                    String cd = parameter;
-                    if(!cd.startsWith("/")) {
-                        if(!cd.equals("~")) {
-                            cd = dir + "/" + parameter;
+                    if (arguments.length == 0) {
+                        System.out.println("cd: missing argument");
+                        break;
+                    }
+                    String cd = arguments[0];
+                    if (!cd.startsWith("/")) {
+                        if (!cd.equals("~")) {
+                            cd = dir + "/" + arguments[0];
                         } else {
                             cd = System.getenv("HOME");
                         }
                     }
 
-                    if(Files.isDirectory(Path.of(cd))) {
+                    if (Files.isDirectory(Path.of(cd))) {
                         dir = Path.of(cd).normalize().toString();
                     } else {
                         System.out.println("cd: " + cd + ": No such file or directory");
                     }
                     break;
                 default:
-                    if(!parameter.equals("")) {
-                        String path = getPath(command);
-                        if(path != null) {
-                            String[] fullPath = new String[]{command, parameter};
-                            Process process = Runtime.getRuntime().exec(fullPath);
-                            process.getInputStream().transferTo(System.out);
-                        } else {
-                            System.out.println(command + ": command not found");
-                        }
+                    String path = getPath(command);
+                    if (path != null) {
+                        String[] fullPath = new String[tokens.size()];
+                        fullPath[0] = path;
+                        System.arraycopy(arguments, 0, fullPath, 1, arguments.length);
+                        Process process = Runtime.getRuntime().exec(fullPath);
+                        process.getInputStream().transferTo(System.out);
                     } else {
-                        System.out.println(input + ": command not found");
+                        System.out.println(command + ": command not found");
                     }
             }
         }
@@ -91,7 +84,7 @@ public class Main {
     private static String getPath(String parameter) {
         for (String path : System.getenv("PATH").split(":")) {
             Path fullPath = Path.of(path, parameter);
-            if( Files.isRegularFile(fullPath)) {
+            if (Files.isRegularFile(fullPath)) {
                 return fullPath.toString();
             }
         }
@@ -99,12 +92,31 @@ public class Main {
     }
 
     private static List<String> builtins() {
-        List<String> builtins = new ArrayList<>();
-        builtins.add("exit");
-        builtins.add("echo");
-        builtins.add("type");
-        builtins.add("pwd");
-        builtins.add("cd");
-        return builtins;
+        return Arrays.asList("exit", "echo", "type", "pwd", "cd");
+    }
+
+    private static List<String> parseInput(String input) {
+        List<String> tokens = new ArrayList<>();
+        boolean inQuotes = false;
+        StringBuilder currentToken = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (c == '\'') {
+                inQuotes = !inQuotes; // Toggle quote mode
+            } else if (c == ' ' && !inQuotes) {
+                if (currentToken.length() > 0) {
+                    tokens.add(currentToken.toString());
+                    currentToken.setLength(0);
+                }
+            } else {
+                currentToken.append(c);
+            }
+        }
+
+        if (currentToken.length() > 0) {
+            tokens.add(currentToken.toString());
+        }
+
+        return tokens;
     }
 }
