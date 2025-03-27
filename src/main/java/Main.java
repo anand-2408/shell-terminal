9d785840caf4c0c4c75c4b91a7251c68ab604c80
Anand Sherikar <anand007sherikar@gmail.com>
2025-03-27 14:10:09 +0530

    echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         while(true){
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
-            String input =scanner.nextLine();
+            String input =scanner.nextLine().trim();
 
             if(input.equals("exit 0")){
                 break;
