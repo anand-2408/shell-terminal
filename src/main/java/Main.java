286c2e560a92f55f1de305b9cb4253ff90f9db5a
Anand Sherikar <anand007sherikar@gmail.com>
2025-03-27 14:08:37 +0530

    echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+
 
         while(true){
             System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
             String input =scanner.nextLine();
 
             if(input.equals("exit 0")){
