d19105b032af6bb839a08ae7a7622a4096749322
Anand Sherikar <anand007sherikar@gmail.com>
2025-03-27 13:55:52 +0530

    exit builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("$ ");
+
         while(true){
+            System.out.print("$ ");
             String input =scanner.nextLine();
 
             if(input.equals("exit 0")){
