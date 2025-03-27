b4c33f6db6fd832829a41dd5eb026316dce2d33d
Anand Sherikar <anand007sherikar@gmail.com>
2025-03-27 14:14:07 +0530

    echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
+import javax.swing.plaf.TreeUI;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        Scanner scanner = new Scanner(System.in);
 
         while(true){
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            String input =scanner.nextLine().trim();
+
+            String input =scanner.nextLine();
 
             if(input.equals("exit 0")){
                 break;
             }
-            if(input.startsWith("echo")){
-                System.out.println(input.subString(5));
+            else if(input.startsWith("echo")){
+                System.out.println(input.subString(input.indexOf("echo ")+5));
             }
             else{
                 System.out.println(input + ": command not found");
