01f9bd39d484810ced8ef8aefbb7faf852f4b94f
Anand Sherikar <anand007sherikar@gmail.com>
2025-03-27 14:23:02 +0530

    type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,38 @@
 import java.util.Scanner;
-import javax.swing.plaf.TreeUI;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
+        String input, typeSubString;
+        String[] commands = {"echo", "exit", "type"};
 
         while(true){
             System.out.print("$ ");
 
-            String input =scanner.nextLine();
+            input =scanner.nextLine();
 
             if(input.equals("exit 0")){
                 break;
             }
             else if(input.startsWith("echo ")){
-                System.out.println(input.subString(input.indexOf("echo ")+5));
+                System.out.println(input.subString(5));
+            }
+            else if(input.startsWith("type ")){
+                typeSubString = input.subString(5);
+                if (Arrays.asList(commands).contains(typeSubstring)) {
+                    System.out.println(typeSubstring + " is a shell builtin");
+                }
+                else {
+                    System.out.println(typeSubstring + " not found");
+                }
             }
             else{
                 System.out.println(input + ": command not found");
             }
 
         }
-
-
     }
     //scanner.close();
 }
