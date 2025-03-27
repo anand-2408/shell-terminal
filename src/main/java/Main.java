f29870ec4d30656d8f5bb613f6c277f445b44fba
Anand Sherikar <anand007sherikar@gmail.com>
2025-03-27 15:48:15 +0530

    executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,66 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        String input, typeSubstring;
-        String[] commands = {"echo", "exit", "type"};
+        List<String> builtins = builtins();
+        String input;
 
         while (true) {
             System.out.print("$ ");
 
             input = scanner.nextLine();
+            String[] str = input.split(" ");
+            String command = str[0];
+            String parameter = "";
 
-            if (input.equals("exit 0")) {
-                break;
-            } else if (input.startsWith("echo")) {
-                System.out.println(input.substring(5));  // Fixed: changed subString to substring
-            } else if (input.startsWith("type")) {
-                typeSubstring = input.substring(5);  // Fixed: changed subString to substring
-                if (Arrays.asList(commands).contains(typeSubstring)) {  // Fixed: Corrected variable name
-                    System.out.println(typeSubstring + " is a shell builtin");
-                } else {
-                    System.out.println(typeSubstring + " not found");
+            if (str.length > 2) {
+                for (int i = 1; i < str.length; i++) {
+                    if (i < str.length - 1) {
+                        parameter += str[i] + " ";
+                    } else {
+                        parameter += str[i];
+                    }
                 }
-            } else {
-                System.out.println(input + ": command not found");
+            }
+            else if (str.length > 1) {
+                parameter = str[1];
+            }
+
+            switch (command) {
+                case "exit":
+                    if (parameter.equals("0")) {
+                        System.exit(0);
+                    } else {
+                        System.out.println(input + ": command not found");
+                    }
+                    break;
+                case "echo":
+                    System.out.println(parameter);
+                    break;
+                case "type":
+                    if (parameter.equals(builtins(get(0)) || parameter.equals(builtins.get(1)) || parameter.equals(builtins.get(2)))) {
+                        System.out.println(parameter + ": is a shell builtin");
+                    } else {
+                        String path = getPath(parameter);
+                        if (path != null) {
+                            System.out.println(parameter + " is " + path);
+                        } else {
+                            System.out.println(parameter + ": not found");
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println(input + ": command not found");
             }
         }
-
-        scanner.close();  // Closing scanner to avoid resource leak
     }
+    
+
+
 }
